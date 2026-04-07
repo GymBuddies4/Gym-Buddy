0100ff9f173acbe93e4570e058bd78469e1e1f93
Prudy8 <DeborahMcDougall10@gmail.com>
2026-04-06 23:19:51 -0400

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -2,6 +2,15 @@ import typer
 from app.database import get_cli_session
 from app.models.user import User
 
-app = typer.Typer()
+cli = typer.Typer()
 
 @cli.command()
+def user():
+    with get_cli_session() as session:
+        users = [
+            User(username = "bob", email="bobname@info2602.com", password = "bobpass", role = "user"),
+        ]
+        for user in users:
+            session.add(user)
+            session.commit()
+            print("You have succesfully logged in!")

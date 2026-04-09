53f9258eaf05eb17a3c468403bfb3edc3487bc3b
Prudy8 <DeborahMcDougall10@gmail.com>
2026-04-09 19:00:21 -0400

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -1,6 +1,7 @@
 import typer
 from app.database import get_cli_session
 from app.models.user import User
+from app.utilities.security import encrypt_password
 
 cli = typer.Typer()
 
@@ -8,9 +9,9 @@ cli = typer.Typer()
 def user():
     with get_cli_session() as session:
         users = [
-            User(username = "bob", email="bobname@info2602.com", password = "bobpass", role = "user"),
+            User(username = "bob", email="bobname@info2602.com", password=encrypt_password("bobpass"), role = "user"),
         ]
         for user in users:
             session.add(user)
-            session.commit()
-            print("You have succesfully logged in!")
+        session.commit()
+        print("You have succesfully logged in!")
